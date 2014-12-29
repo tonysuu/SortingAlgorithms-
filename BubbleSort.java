f3d69ebb2dd88e61206c2c0de5d7f508b5d53b59
tonysuu <tonysu1122@hotmail.com>
2014-12-29 18:13:58 -0500

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+import hsa.*;
+
+public class BubbleSort {
+    public static void main(String [] args){
+        int array [] = {1,5,3,2,9};
+        Sort(array);
+    }
+    public static void Sort(int [] array)
+    {
+        int temp = 0;
+        for (int i = array.length-1; i >= 0; i--)
+        {
+            for (int a = 0; a < i; a++)
+            {
+                if (array[a] > array[a+1])
+                {
+                    temp = array[a];
+                    array[a] = array[a+1];
+                    array[a+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
+//sort a list of numbers in ascending order.
